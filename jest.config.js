083dc0fc526cb1eb37ab70147c5a81084cc4e9fe
Anurag Rajawat <anurag@stepsecurity.io>
2026-06-29 17:14:44 +0530

    fix: Resolve security vulnerabilities
    Signed-off-by: Anurag Rajawat <anurag@stepsecurity.io>

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = async () => {
-    return {
-        // verbose: true,
-        testEnvironment: "node",
-        transform: {
-            "^.+\\.jsx?$": "babel-jest"
-        },
-    };
-};
